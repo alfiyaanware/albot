b87168ca12a3e1fbcd3c31ac109a81db7243dc62
Alfiya Anware <99505806+alfiyaanware@users.noreply.github.com>
2023-12-30 12:42:07 +0530

    additions

--- albot-web/src/pages/index.js
+++ albot-web/src/pages/index.js
@@ -1,5 +1,44 @@
+import {useState} from "react";
+
 export default function Home() {
-  return (
-    <div>Hello World</div>
-  )
+  const [apiKey, setApiKey] = useState(''); 
+
+  const API_URL= "https://api.openai.com/v1/chat/completions";
+
+  async function sendRequest(){
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers:{
+        "Content-Type":"application/json",
+        Authorization:"Bearer "+ apiKey,
+      },
+      body:JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Hello."}]
+      }),
+    });
+
+    const responseJson= await response.json();
+     console.log(responseJson);
+  }
+  return <div className="flex flex-col h-screen">
+    <nav className="shadow p-4 flex flex-row justify-between items-center">
+      <div className="text-xl font-bold">Albot</div>
+      <div>
+        <input type="password" 
+        className="border p-1 rounded" 
+        onChange={e => setApiKey(e.target.value)}
+        value={apiKey}
+        placeholder="Paste API Key here" />
+      </div> 
+    </nav>
+
+    <div className="p-4">
+      <button className="border rounded-md p-2 bg-pink-500 hover:bg-black text-white"
+      onClick={sendRequest}>
+        Send Request
+      </button>
+    </div>
+  </div>
 }
+ 
