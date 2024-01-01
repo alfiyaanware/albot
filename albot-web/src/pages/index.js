811439cdfa872c7afc9193cf51db62f695f2c202
Alfiya Anware <99505806+alfiyaanware@users.noreply.github.com>
2024-01-01 15:34:33 +0530

    streaming

--- albot-web/src/pages/index.js
+++ albot-web/src/pages/index.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import ReactMarkdown from 'react-markdown';
 import Head from "next/head";
+import { createParser } from "eventsource-parser";
 
 const SYSTEM_MESSAGE= "You are Albot, a helpful and versatile AI created by Alfiya Anware using state of the art ML models and APIs."
 
@@ -12,32 +13,67 @@ export default function Home() {
 
   const API_URL= "https://api.openai.com/v1/chat/completions";
 
-  async function sendRequest(){
+  const sendRequest = async () => {
+    const updatedMessages = [...messages, {role: "user", content: userMessage,},];
 
-    const newMessages =[...messages, {role:"user", content: userMessage}]
-
-    setMessages(newMessages);
+    setMessages(updatedMessages);
     setUserMessage("");
 
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers:{
-        "Content-Type":"application/json",
-        Authorization:"Bearer "+ apiKey,
-      },
-      body:JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: newMessages,
-      }),
-    });
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: updatedMessages,
+          stream: true,
+        }),
+      });
 
-    const responseJson= await response.json();
+      const reader = response.body.getReader();
 
-    const newBotMessage = responseJson.choices[0].message;
-    const newMessages2 = [...newMessages, newBotMessage]
-    setMessages(newMessages2);
-    
-  }
+      let newMessage = "";
+      const parser = createParser((event) => {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            return;
+          }
+          const json = JSON.parse(event.data);
+          const content = json.choices[0].delta.content;
+
+          if (!content) {
+            return;
+          }
+
+          newMessage += content;
+
+          const updatedMessages2 = [
+            ...updatedMessages,
+            { role: "assistant", content: newMessage },
+          ];
+
+          setMessages(updatedMessages2);
+        } else {
+          return "";
+        }
+      });
+
+      // eslint-disable-next-line
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const text = new TextDecoder().decode(value);
+        parser.feed(text);
+      }
+    } catch (error) {
+      console.error("error");
+      window.alert("Error:" + error.message);
+    }
+  };
 
   return (
   
